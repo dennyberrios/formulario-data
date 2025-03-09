01f838d5578a9d9b0df386b515f0d929d7b6cda6
Danny Berrios <denny_berrios@hotmail.com>
2025-03-09 00:56:32 -0300

    Adiciona estilos globais ao projeto

---
+++ front-end/src/components/UI/GlobalStyles/index.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        outline: none;
+        text-decoration: none;
+        list-style: none;
+        border: none;
+        box-sizing: border-box;
+        background-color: gray;
+    }
+`;
