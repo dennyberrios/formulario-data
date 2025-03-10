b9846802fb37d6d494c1802d5c9cd17893ab3292
Danny Berrios <denny_berrios@hotmail.com>
2025-03-09 23:47:57 -0300

    Adiciona suporte a temas com ThemeProvider e configura rotas na aplicação

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,15 @@
-import { GlobalStyle } from "./components/UI/GlobalStyles"
+import { GlobalStyle } from "./components/UI/GlobalStyles";
+import RoutesApp from "./routes";
+import { theme } from './components/UI/Theme/theme';
+import { ThemeProvider } from "styled-components";
 
 function App() {
   return (
-    <>
-    <GlobalStyle />
-     app
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RoutesApp />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
