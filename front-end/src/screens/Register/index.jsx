85f3dad7a7c8f9096306ccafd92f99fbbc9ee76f
Danny Berrios <denny_berrios@hotmail.com>
2025-03-09 23:46:08 -0300

    Adiciona tela de registro com formulário de informações do usuário e estilos

---
+++ front-end/src/screens/Register/index.jsx
@@ -0,0 +1,13 @@
+
+import UserInfoForm from "../../components/UserInfoForm";
+import { Container } from "./styles";
+
+const Register = () => {
+  return (
+  <Container>
+    <UserInfoForm/>
+  </Container>
+);
+};
+
+export default Register;
