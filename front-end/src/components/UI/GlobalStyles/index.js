c0d0050b27358db0a0c8b46b5d5f0ff97f1882b0
Danny Berrios <denny_berrios@hotmail.com>
2025-03-09 23:47:04 -0300

    Adiciona estilos globais com tema e configurações de tipografia

--- front-end/src/components/UI/GlobalStyles/index.js
+++ front-end/src/components/UI/GlobalStyles/index.js
@@ -1,14 +1,32 @@
 import { createGlobalStyle } from "styled-components";
+import { theme } from "../Theme/theme";
 
 export const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
-        outline: none;
-        text-decoration: none;
-        list-style: none;
-        border: none;
         box-sizing: border-box;
-        background-color: gray;
+    }
+
+    body {
+        font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+        Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        background-color: ${theme.colors.background};
+        color: ${theme.colors.text};
+        line-height: 1.5;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    button {
+        cursor: pointer;
+        font-family: inherit;
+    }
+
+    input, button {
+        outline: none;
     }
 `;
