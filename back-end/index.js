602267895a66fd56399e07354997e62ace7566fd
Danny Berrios <denny_berrios@hotmail.com>
2025-03-11 22:55:41 -0300

    Adiciona configuração básica do servidor Express e rota de usuários

---
+++ back-end/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import users from './routes/users.js'
+
+const app = express();
+const port = 3000;
+
+app.use(express());
+app.use(express.json());
+app.use(cors());
+
+app.use("/users", users)
+
+app.use((req, res, next) => {
+    res.status(500).send({
+        error: "Rota não encontrada"
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta: http://localhost:${port}`);
+});
