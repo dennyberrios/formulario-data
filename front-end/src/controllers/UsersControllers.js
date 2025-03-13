d6235659a65c0784733582d8462639b150f20e10
Danny Berrios <denny_berrios@hotmail.com>
2025-03-12 23:52:38 -0300

    Adiciona controlador de usuários com métodos para CRUD utilizando apiControllers

---
+++ front-end/src/controllers/UsersControllers.js
@@ -0,0 +1,55 @@
+import apiControllers from "./apiControllers";
+
+class UsersController {
+  getUsers = async () => {
+    try {
+      const response = await apiControllers.Search(`/users/search`);
+      return response;
+    } catch (error) {
+      console.error({ MSG: "Error get data:", error });
+      throw error;
+    }
+  };
+
+  getUsersById = async (id) => {
+    try {
+      const response = await apiControllers.Search(`/users/search/${id}`);
+      return response;
+    } catch (error) {
+      console.error({ MSG: "Error get data:", error });
+      throw error;
+    }
+  };
+
+  createUser = async (data) => {
+    try {
+      const response = await apiControllers.Create(`/users/create`, data);
+      return response;
+    } catch (error) {
+      console.error({ MSG: "Error create data:", error });
+      throw error;
+    }
+  };
+
+  updateUser = async (id, data) => {
+    try {
+      const response = await apiControllers.Update(`/users/update/${id}`, data);
+      return response;
+    } catch (error) {
+      console.error({ MSG: "Error update data:", error });
+      throw error;
+    }
+  };
+
+  deleteUser = async (id) => {
+    try {
+      const response = await apiControllers.Delete(`/users/delete/${id}`);
+      return response;
+    } catch (error) {
+      console.error({ MSG: "Error delete data:", error });
+      throw error;
+    }
+  };
+}
+
+export default new UsersController();
