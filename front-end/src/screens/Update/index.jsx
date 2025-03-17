d32e5849953e58d0ff2bb9217ff07e2ecb5767a7
Danny Berrios <denny_berrios@hotmail.com>
2025-03-16 22:46:31 -0300

    formata o componente Update para melhorar a legibilidade do código

--- front-end/src/screens/Update/index.jsx
+++ front-end/src/screens/Update/index.jsx
@@ -2,11 +2,12 @@ import UserInfoForm from "../../components/UserInfoForm";
 import { Container } from "./styles";
 
 const Update = () => {
-    return ( 
-        <Container>
-            <UserInfoForm />
-        </Container>
-     );
-}
- 
+
+  return (
+    <Container>
+      <UserInfoForm />
+    </Container>
+  );
+};
+
 export default Update;
