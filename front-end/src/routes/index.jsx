b6d90dbff83818c1537430e41c764e29b6335155
Danny Berrios <denny_berrios@hotmail.com>
2025-03-21 22:45:27 -0300

    refatora rotas para utilizar componente Endpoints

--- front-end/src/routes/index.jsx
+++ front-end/src/routes/index.jsx
@@ -1,20 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '../screens/Home';
-import Header from '../components/Header';
-import Register from '../screens/Register';
-import Update from '../screens/Update';
+import { BrowserRouter as Router } from "react-router-dom";
+import Endpoints from "./endpoints";
 
 const RoutesApp = () => {
-    return ( 
-        <Router>
-            <Header/>
-            <Routes>
-                <Route path='/' element={<Home />}/>
-                <Route path='/register' element={<Register/>} />
-                <Route path='/edit/:id' element={<Update/>} />
-            </Routes>
-        </Router>
-     );
-}
- 
+  return (
+    <Router>
+      <Endpoints />
+    </Router>
+  );
+};
+
 export default RoutesApp;
