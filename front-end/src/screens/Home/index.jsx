46adfcc3cadcca6068c49bb83939568fe7887d8c
Danny Berrios <denny_berrios@hotmail.com>
2025-03-21 22:45:17 -0300

    adiciona mensagem de não encontrado na tela inicial ao exibir usuários

--- front-end/src/screens/Home/index.jsx
+++ front-end/src/screens/Home/index.jsx
@@ -2,24 +2,29 @@ import { useContext } from "react";
 import { Container, UserGrid } from "./styles";
 import { UserContext } from "../../Context/context";
 import CardUser from "../../components/CardUser";
+import NotFoundMessage from "../../components/NotFoundMessage/NotFoundMessage";
 
 const Home = () => {
-  const {data} = useContext(UserContext);
+  const { data } = useContext(UserContext);
 
   return (
     <Container>
-      <UserGrid>
-        {data.map((user) => (
-          <CardUser
-            key={user.id_user}
-            id={user.id_user}
-            name={user.name}
-            email={user.email}
-            cpf={user.cpf}
-            dateOfBirth={user.dateOfBirth}
-          />
-        ))}
-      </UserGrid>
+      {data && data.length > 0 ? (
+        <UserGrid>
+          {data.map((user) => (
+            <CardUser
+              key={user.id_user}
+              id={user.id_user}
+              name={user.name}
+              email={user.email}
+              cpf={user.cpf}
+              dateOfBirth={user.dateOfBirth}
+            />
+          ))}
+        </UserGrid>
+      ) : (
+        <NotFoundMessage />
+      )}
     </Container>
   );
 };
