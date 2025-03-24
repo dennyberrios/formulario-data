fd5941165c596e4d9dc3558b6f3108765d50d0c0
Danny Berrios <denny_berrios@hotmail.com>
2025-03-23 23:52:53 -0300

    adiciona função de login com tratamento de erros e redirecionamento

--- front-end/src/screens/Login/index.jsx
+++ front-end/src/screens/Login/index.jsx
@@ -9,64 +9,69 @@ import {
 } from "./styles";
 import { UserContext } from "../../Context/context";
 import { useNavigate } from "react-router-dom";
-import {
-  validateEmail,
-  validatePassword,
-} from "../../utils/validation";
+import { validateEmail, validatePassword } from "../../utils/validation";
 import InputLabel from "../../components/InputLabel";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const { setReloadLayout, reloadLayout } = useContext(UserContext);
-  
-    const [formData, setFormData] = useState({
-      email: "",
-      password: "",
-    });
-  
-    const [errors, setErrors] = useState({});
-  
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormData({ ...formData, [name]: value });
-  
-      // Remove erro individual ao corrigir o campo
-      const fieldError = validateField(name, value);
-      setErrors({ ...errors, [name]: fieldError });
-    };
-  
-    const validateField = (name, value) => {
-      switch (name) {
-        case "email":
-          return validateEmail(value);
-        case "password":
-          return validatePassword(value);
-        default:
-          return null;
+  const navigate = useNavigate();
+  const { setReloadLayout, reloadLayout, login } = useContext(UserContext);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+
+    // Remove erro individual ao corrigir o campo
+    const fieldError = validateField(name, value);
+    setErrors({ ...errors, [name]: fieldError });
+  };
+
+  const validateField = (name, value) => {
+    switch (name) {
+      case "email":
+        return validateEmail(value);
+      case "password":
+        return validatePassword(value);
+      default:
+        return null;
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      setErrors({
+        email: validateEmail(formData.email),
+        password: validatePassword(formData.password),
+      });
+      return;
+    }
+
+    try {
+      const response = await login(formData);
+      if (response?.status) {
+        // Aqui poderia chamar um serviço de autenticação
+        console.log("Usuário logado com sucesso");
+        navigate("/home");
+        console.log(response.result[0]);
+        setReloadLayout(!reloadLayout);
+      } else {
+        console.log("Usuário não encontrado");
       }
-    };
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      if (!formData.email || !formData.password) {
-        setErrors({
-          email: validateEmail(formData.email),
-          password: validatePassword(formData.password),
-        });
-        return;
-      }
-  
-      // Simulação de login bem-sucedido
-      console.log("✅ Login realizado com:", formData);
-  
-      // Aqui poderia chamar um serviço de autenticação
-      setReloadLayout(!reloadLayout);
-      navigate("/home");
-    };
+    } catch (error) {
+      console.error({ MSG: "Error login:", error });
+    }
+  };
 
   return (
-<Container>
+    <Container>
       <FormContainer>
         <Title>Login</Title>
         <Form onSubmit={handleSubmit}>
